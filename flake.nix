2c9c3d6283afc982fb0fc9a7d62090de3dc9a5e2
Daniel Lu <daniel@danielclu.com>
2023-11-23 19:45:16 -0500

    there's some commented out hidpi junk here

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "nixos configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      daniel = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.daniel = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
