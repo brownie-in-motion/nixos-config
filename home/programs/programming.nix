90e2eb7b181382ee39811ebcb946c1baad9f387b
Daniel Lu <daniel@danielclu.com>
2023-12-19 18:46:41 -0800

    feat: add some things to programming.nix

--- home/programs/programming.nix
+++ home/programs/programming.nix
@@ -2,6 +2,18 @@
 
 {
   home.packages = with pkgs; [
+    bun
+    deno
+    nodejs_21
+
+    ghc
+    ghcid
+
+    python311Packages.python
+    python311Packages.pillow
+    python311Packages.requests
+    python311Packages.z3
+
     ocaml
     ocamlPackages.dune_3
   ];
